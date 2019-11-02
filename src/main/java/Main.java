de8ae327c7b04fc533ba23eaed0704ca6c3d82d0
Lostr9 <lostr9@icloud.com>
2019-11-03 00:26:55 +0300

    Создана начальная структура приложения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import engine.GameEngine;
+import engine.GameLogic;
+import game.DummyGame;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
@@ -13,103 +16,17 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
 
-    // Оконный обработчик
-    private long window;
-
-    public void run() {
-        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
-
-        init();
-        loop();
-
-        // Free the window callbacks and destroy the window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        // Terminate GLFW and free the error callback
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
-    }
-
-    private void init() {
-        // Настройка обратного вызова по ошибке. Реализация по умолчанию
-        // выведет сообщение об ошибке в System.err.
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        // Инициализируйте GLFW. Большинство функций GLFW не будут работать до этого.
-        if ( !glfwInit() )
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        // Настройка параметров GLFW
-        glfwDefaultWindowHints(); // опционально, текущие подсказки к окну уже являются подсказками по умолчанию
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // после создания окно останется скрытым
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // окно будет изменять размер
-
-        // Создание окна
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        // Настройка обратного вызова клавиш. Он будет вызываться при каждом нажатии, повторном нажатии или отпускании кнопки.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                glfwSetWindowShouldClose(window, true); // Мы обнаружим это в цикле рендеринга.
-        });
-
-        // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Получить размер окна, переданного в glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Получение разрешения первичного монитора
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Расположите окно по центру
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make the window visible
-        glfwShowWindow(window);
-    }
-
-    private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
-        GL.createCapabilities();
-
-        // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
-        while ( !glfwWindowShouldClose(window) ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-            glfwSwapBuffers(window); // swap the color buffers
-
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents();
-        }
-    }
-
     public static void main(String[] args) {
-        new Main().run();
+        try {
+            boolean vSync = true;
+            GameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME",
+                    600, 480, vSync, gameLogic);
+            gameEng.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
 }
